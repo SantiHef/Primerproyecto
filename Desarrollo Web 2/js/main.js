c376539efdab13820eacde2ad6c9c43c5971effa
SantiHef <166764214+SantiHef@users.noreply.github.com>
2024-04-18 19:38:55 -0300

    Add files via upload

---
+++ Desarrollo Web 2/js/main.js
@@ -0,0 +1,9 @@
+let nombreProducto = "Heladera"
+let precioUnitario = 300000
+let cantidadDeseada = prompt("Ingrese la Cantidad de Heladeras que quiere")
+ 
+let costoFinal = precioUnitario * cantidadDeseada
+
+console.log(costoFinal);
+
+alert(costoFinal)
