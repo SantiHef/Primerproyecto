d53381ad88a636cd9a5a8f12446aeb56c8b46792
SantiHef <166764214+SantiHef@users.noreply.github.com>
2024-06-02 16:38:52 -0300

    Add files via upload

---
+++ Desarrollo Web 2 - Desafío 6/js/main.js
@@ -0,0 +1,12 @@
+const titulo = document.querySelector(".nombreProducto")
+
+console.log(titulo.innerHTML)
+
+const precio = document.getElementById("precioProducto")
+
+precio.innerText = ("Precio: $450")
+
+console.log(precio.innerText)
+
+
+
